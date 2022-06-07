2d7eb75bb0de19f8d5490e141720b96dec1d3999
zzerooo.twooo <leva22062007@gmail.com>
2022-06-07 13:57:23 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,90 @@
+"use strict";
+
+let number = 4.6 //Number
+
+console.log(4/0); //Infinity
+
+console.log(-4/0); // -Infinity
+
+console.log('string' * 9); //NaN - not a number
+
+const person = 'Alex'; // String
+
+const bool = true or false; //Boolean
+
+console.log(something) //Null Это когда чего то просто нету
+
+let und;
+console.log(und); //Undefined Это когда что то есть, но это значение пустое
+
+
+// Объекты 
+
+// Объекты - это коллекция данных, структура которая используется для хранения абсолютно любых данных
+// Такой тип данных не зря называется комплексным, ведь в нем могут содержаться все типы данных
+
+
+const obj = {
+    //Свойство(ключ):  Значение
+       name: 'John',
+       age: 25,
+       isMarried: false     
+}
+
+
+//console.log(obj.name); //John
+
+console.log(obj[name]); //Error 
+
+console.log(obj['name']);
+
+
+// ARRAY, массив является частным случаем объекта 
+                0          1         2       3        4 //Ключи уже заданы заранее в Массиве
+// let arr = ['plum.png', 'orange.jpg', 7, 'apple.bmp', {}]; 
+
+// console.log(arr[1]); //Orange.jpg
+
+
+
+// const array = [1, 2, 3];
+// const arrObj = {
+
+//     0: 1, 
+//     1: 2,
+//     2: 3
+// };
+
+// console.log(array[1]); // 
+
+// const Obj = {
+//     'Anna': 500,
+//     'Alice': 800
+// };
+
+
+const result = confirm('');
+
+alert('hello world');
+
+console.log('arr' + ' - object');
+
+
+
+
+let incr = 10;
+let decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr); // 10 если мы поставим в постфиксную форму то консоль выдаст 10, потому что она сначала возвращает значение а потом увеличивает или уменьшает
+console.log(--decr); // 
+
+
+
+
+
+
+
+
