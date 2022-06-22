647d248dbc2b57d79d60639cbe317de7bc6a51eb
zzerooo.twooo <leva22062007@gmail.com>
2022-06-22 13:46:00 +0200

    added methods and arrays

--- js/index.js
+++ js/index.js
@@ -583,3 +583,105 @@
 // function compsreNum(a, b) {
 //     return a - b;
 // }
+
+
+// (35 урок)
+
+// let a = 5,
+//     b = a; /// Это работает только с примитивами, только простые тп ы данных передаются по значению
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {  //  В обьектах это передача по ссылке, она передает не саму структуру обьекта а именно ссылку
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function Copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add); // первый аргумент это то во что мы помещаем, а второй, что именно мы помещаем
+
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+
+// const oldArr = ['a', 'b', 'c'];
+// const newArr = oldArr.slice(); 
+
+
+// newArr[1] = 'mrknckn';
+// console.log(newArr);
+// console.log(oldArr);
+
+// // spread
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];  // оператор разворота развернул структуру на отдельные элементы
+
+//       console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+
+// const newArrt = [...array];
+
+// const OObj = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newwObj = {...OObj};
